1e386b6c3e18d664aa0c36b47b965c91c79c57dd
YmerA38 <stin166c@stud.kea.dk>
2023-11-21 22:31:27 +0100

    in progess

---
+++ src/main/java/com/ways/krbackend/constant/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.ways.krbackend.constant;
+
+public interface SecurityConstants {
+    public static final String JWT_KEY ="22c71a71-a65c-4e8d-8996-00589d9981fe";
+    public static final String JWT_HEADER ="Authorization";
+}
