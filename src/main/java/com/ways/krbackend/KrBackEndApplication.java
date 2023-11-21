a3298cd39b95f94a2153b3d2cb11eab24979fac6
YmerA38 <stin166c@stud.kea.dk>
2023-11-21 10:25:59 +0100

    in progess

---
+++ src/main/java/com/ways/krbackend/KrBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ways.krbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ways/krbackend/KrBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ways.krbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
