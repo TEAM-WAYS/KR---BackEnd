96ea8428195854b453fc0144a0cd35c15189512c
YmerA38 <stin166c@stud.kea.dk>
2023-11-23 12:01:23 +0100

    in progess

--- src/main/java/com/ways/krbackend/config/CorsConfig.java
+++ src/main/java/com/ways/krbackend/config/CorsConfig.java
@@ -5,11 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Replace with the origin of your frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
-
-        @Bean
+     /*   @Bean
         public CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration configuration = new CorsConfiguration();
             configuration.setAllowCredentials(true);
@@ -22,7 +36,7 @@ public class CorsConfig {
             source.registerCorsConfiguration("/**", configuration);
 
             return source;
-        }
+        }*/
 
 
 }
--- src/main/java/com/ways/krbackend/controller/UserController.java
+++ src/main/java/com/ways/krbackend/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin
 public class UserController {
     @Autowired
     UserService userService;
