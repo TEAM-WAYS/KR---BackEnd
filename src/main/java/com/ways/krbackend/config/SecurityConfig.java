c578127991ca3e8b518091b0460676be943afa2e
YmerA38 <stin166c@stud.kea.dk>
2023-12-13 19:32:06 +0100

    problem fixed

--- src/main/java/com/ways/krbackend/config/SecurityConfig.java
+++ src/main/java/com/ways/krbackend/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
                     .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
                     .authorizeHttpRequests((requests)->requests
                             //add to whitelist for authenticated sites
-                            .requestMatchers("/application","/application/**","/application/**/**").authenticated()
+                            .requestMatchers("/application","/application/**","/application/search/**").authenticated()
                             .requestMatchers("/emails","/emails/**").authenticated()
                             .requestMatchers("/new-user","/login-user").permitAll()
                     )
